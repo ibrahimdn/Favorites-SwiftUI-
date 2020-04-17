f16dd90407b070e2902c54ccc00305c81524aa6e
İbrahim DOĞAN <ibrahim.dogan@btpro.net>
2020-04-17 10:38:46 +0300

    Initial Commit

---
+++ Favorite(SwiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favorite(SwiftUI)
+//
+//  Created by İbrahim DOĞAN on 4/17/20.
+//  Copyright © 2020 ibrahim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
